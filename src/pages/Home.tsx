23036a5ef9f305a4f6e22267357820a4bc606fcb
Cheng Yang <chengyang.ong@syntrinos.com.my>
2021-08-10 16:19:23 +0800

    update layout

---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+import React,{useEffect,useState} from 'react';
+import Loading from '../components/Loading';
+import MasterPageLayout from '../components/masterPage';
+
+const HomePage=()=>{
+
+    return (
+        <MasterPageLayout>
+            <div className="bodyWrapper">
+                Helloo
+            </div>
+        </MasterPageLayout>
+    )
+}
+
+export default HomePage
